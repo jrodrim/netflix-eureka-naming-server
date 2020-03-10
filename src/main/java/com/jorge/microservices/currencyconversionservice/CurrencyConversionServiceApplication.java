6bd37c3827d850182baf5b5210df48100ad41b99
jrodrim <jorgecarl.6@gmail.com>
2020-03-10 15:12:55 +0100

    aggiuinto client discovery eureka

--- src/main/java/com/jorge/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/jorge/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.jorge.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.jorge.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication
 {
 
